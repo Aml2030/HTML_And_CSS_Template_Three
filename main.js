e099335d213f9f9b709aa2834de6bd511525ac38
Aml2030 <118318264+Aml2030@users.noreply.github.com>
2023-03-03 17:39:05 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Increase Numbers On Scrolling
+
 let section = document.querySelector(".stats");
 let numsArr = document.querySelectorAll(".number");
 let started = false;
@@ -20,3 +22,29 @@ function startIncrement(el) {
     }
   }, 3500 / goal);
 }
+
+
+// Create Countdown Timer
+
+let countDownDate = new Date("Dec 31, 2023 23:59:59").getTime();
+
+let counter = setInterval(() => {
+  let dateNow = new Date().getTime();
+  let diff = countDownDate - dateNow;
+
+  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
+  document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
+  document.querySelector(".minutes").innerHTML =
+    minutes < 10 ? `0${minutes}` : minutes;
+  document.querySelector(".seconds").innerHTML =
+    seconds < 10 ? `0${seconds}` : seconds;
+
+  if (diff < 0) {
+    clearInterval(counter);
+  }
+}, 1000);
