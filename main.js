644e0fc3dffaf6f03cc7600de02fe31f3d748e6c
Aml2030 <118318264+Aml2030@users.noreply.github.com>
2023-03-03 17:44:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -37,11 +37,11 @@ let counter = setInterval(() => {
   let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
-  document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
-  document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
-  document.querySelector(".minutes").innerHTML =
+  document.querySelector(".time .days").innerHTML = days < 10 ? `0${days}` : days;
+  document.querySelector(".time .hours").innerHTML = hours < 10 ? `0${hours}` : hours;
+  document.querySelector(".time .minutes").innerHTML =
     minutes < 10 ? `0${minutes}` : minutes;
-  document.querySelector(".seconds").innerHTML =
+  document.querySelector(".time .seconds").innerHTML =
     seconds < 10 ? `0${seconds}` : seconds;
 
   if (diff < 0) {
