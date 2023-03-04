feb112d20d3e2f87c30de1f16b6c92b2425b4c2a
Aml2030 <118318264+Aml2030@users.noreply.github.com>
2023-03-04 20:15:18 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
-// Increasing the width while scrolling SKILLS
 
-let section = document.querySelector(".our-skills");
-let spans = document.querySelectorAll(".skills .the-progress span");
-
-window.onscroll = function () {
-  if (window.scrollY >= section.offsetTop) {
-    spans.forEach((s) => {
-      s.style.width = s.dataset.width;
-    });
-  }
-};
 
 // Increase Numbers On Scrolling STATS
 
