a1751078281b1165a0e753c2d579c93d81c6c64f
Aml2030 <118318264+Aml2030@users.noreply.github.com>
2023-03-04 20:12:56 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Increasing the width while scrolling SKILLS
 
 let section = document.querySelector(".our-skills");
-let spans = document.querySelectorAll(".the-progress span");
+let spans = document.querySelectorAll(".skills .the-progress span");
 
 window.onscroll = function () {
   if (window.scrollY >= section.offsetTop - 220) {
